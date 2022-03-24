95cb83d9b95335c034f23cc10bc5c3a97800f990
michaeliyke <ymichaelc@gmail.com>
2022-03-24 10:23:00 +0100

    indexing - cleaning up

--- challenges/indexing.py
+++ challenges/indexing.py
@@ -9,11 +9,10 @@ Example r = [ [[1,2,3],2,[1,3]],     [1,2,3]]
 """
 def index(array, value):
   
-  indices_list, trace, depth, level = [], [], 0, 0
+  indices_list, trace = [], []
 
   def indexer(array, value, depth=0, level=0):
     nonlocal trace
-    # if depth > 0: trace.append(level)
     for pos, item in enumerate(array):
       level = pos
       if isinstance(item, list):
@@ -24,10 +23,8 @@ def index(array, value):
         trace.pop()
         continue
       if item != value: continue
-      # if depth > 0 and tace == []: trace.append(level)
       indices_list.append(trace[::])
       indices_list[-1].append(pos)
-    # trace = []
     return indices_list
 
   return indexer(array, value)
